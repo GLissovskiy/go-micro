ac1b859cffc8445352d516d9bee68079cf1897ca
glissovskiy@gmail.com <glissovskiy@gmail.com>
2024-01-02 10:16:56 -0800

    added test for auth-service handlers 11.129

--- authentication-service/cmd/api/setup_test.go
+++ authentication-service/cmd/api/setup_test.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"authentication/data"
 	"os"
 	"testing"
 )
 
+var testApp Config
+
 func TestMain(m *testing.M) {
+
+	repo := data.NewPostgresTestRepository(nil)
+
+	testApp.Repo = repo
+
 	os.Exit(m.Run())
 }
